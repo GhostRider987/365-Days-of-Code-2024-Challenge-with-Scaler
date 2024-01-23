b5fea20e417087ab3fcc91aa92a5596a46ef7283
Abhishek Gupta <110448627+GhostRider987@users.noreply.github.com>
2024-01-23 20:47:07 +0530

    Reverse Bits

--- Problems/Day_022.cpp
+++ Problems/Day_022.cpp
@@ -1,6 +1,6 @@
 unsigned int Solution::reverse(unsigned int A) {
     unsigned int result = 0;
-
+     // Check the ith bit in A, and set the corresponding bit in result
     for (int i = 0; i < 32; ++i) {
         if (A & (1 << i)) {
             result |= (1 << (31 - i));
