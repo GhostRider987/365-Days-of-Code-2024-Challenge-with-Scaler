1c712262b7eba6580dbddb6d38b2ce7cacf51fd7
Abhishek Gupta <110448627+GhostRider987@users.noreply.github.com>
2024-01-27 13:40:31 +0530

    Swap Bits

--- Problems/Day_025.cpp
+++ Problems/Day_025.cpp
@@ -1,8 +1,9 @@
 int Solution::solve(int A, int B, int C) {
+    // Extract the Bth and Cth bits
     int bitB = (A >> (B - 1)) & 1;
     int bitC = (A >> (C - 1)) & 1;
 
-    
+    // XOR to swap the bits if they are different
     if (bitB != bitC) {
         A ^= (1 << (B - 1)) | (1 << (C - 1));
     }
