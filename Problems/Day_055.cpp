dd8daa3399e8002db9b70030d713afbf265d464c
Abhishek Gupta <110448627+GhostRider987@users.noreply.github.com>
2024-02-25 22:20:14 +0530

    Positive Negative

---
+++ Problems/Day_055.cpp
@@ -0,0 +1,14 @@
+vector<int> Solution::solve(vector<int> &A) {
+    int positiveCount = 0;
+    int negativeCount = 0;
+
+    for (int num : A) {
+        if (num > 0) {
+            positiveCount++;
+        } else if (num < 0) {
+            negativeCount++;
+        }
+    }
+
+    return {positiveCount, negativeCount};
+}
