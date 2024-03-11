0350612c52eefa5d585a340d407f043a560af6fc
Abhishek Gupta <110448627+GhostRider987@users.noreply.github.com>
2024-03-11 23:41:28 +0530

    Integer To Roman

---
+++ Problems/Day_070.cpp
@@ -0,0 +1,15 @@
+string Solution::intToRoman(int A) {
+    vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    string result = "";
+
+    for (int i = 0; i < values.size(); i++) {
+        while (A >= values[i]) {
+            result += numerals[i];
+            A -= values[i];
+        }
+    }
+
+    return result;
+}
